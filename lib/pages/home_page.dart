08582989a9de7f4ca748a27ceb9a1ef80953e925
Unicoolo <f2018266170@gmail.com>
2022-03-23 16:03:17 +0500

    Day3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Day 2"),
+        title: Text("Moaven App"),
       ),
       drawer: Drawer(),
       body: Center(
