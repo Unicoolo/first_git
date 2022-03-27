d664f8b9f9257e315c5ef552204ebfc221d0e51a
Unicoolo <f2018266170@gmail.com>
2022-03-27 20:00:09 +0500

    cata

---
+++ lib/models/catalog.dart
@@ -0,0 +1,19 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+
+  Item(this.id, this.name, this.desc, this.price, this.color);
+  }
+
+  final products = [
+  Item(
+  "odepur001",
+  "iPhone 12 Pro",
+  "Apple iPhone 12th generation",
+  999,
+  "#33505a",
+  )
+  ];
